02a873aaf5dbaaf23229bb59ca3f40cda0305527
SmallConfusion <smallconfusion@protonmail.com>
2024-07-27 14:26:02 -0500

    Added attribution page

--- src/music_player/music_player.gd
+++ src/music_player/music_player.gd
@@ -3,12 +3,6 @@ extends AudioStreamPlayer
 @export var play_in_editor := false
 
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	if OS.has_feature("editor") and not play_in_editor:
 		playing = false
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
