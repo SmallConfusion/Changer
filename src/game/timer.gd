4870be32de7fe0a5d1ecffdc4965dd02150fe58f
SmallConfusion <smallconfusion@protonmail.com>
2024-07-28 11:20:42 -0500

    Fixed timer bug

--- src/game/timer.gd
+++ src/game/timer.gd
@@ -3,6 +3,10 @@ extends Label
 @onready var game: Game = get_tree().get_first_node_in_group("game_manager")
 
 
+func _ready() -> void:
+	text = ""
+
+
 func _process(delta: float) -> void:
 	var time = game.get_current_time()
 	var checkpoint = game.get_last_checkpoint_time()
