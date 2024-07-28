070ab3aadb2f0ed80fd14ba712b37d2e5da72747
Fusion <smallconfusion@protonmail.com>
2024-07-28 13:30:01 -0700

    Changed color

--- src/visuals.gd
+++ src/visuals.gd
@@ -4,4 +4,4 @@ extends Node
 static var PENCIL_WIDTH := 10.0
 static var MARKER_WIDTH := 20.0
 
-static var PENCIL_COLOR := Color(0.1, 0.05, 0.1, 0.85)
+static var PENCIL_COLOR := Color(0.2, 0.08, 0.18, 0.55)
